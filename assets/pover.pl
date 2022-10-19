fd3ec19139cc75bd92dff5865e08f5b3f209a7d2
brucemoran <bruce01campus@gmail.com>
2022-10-19 10:04:16 +0100

    pover.pl

--- assets/pover.pl
+++ assets/pover.pl
@@ -16,18 +16,17 @@ open(BED, $ARGV[1]);
 open(OUT, ">$ARGV[2]");
 while(<BED>){
     chomp;
-    my ($chr,$str,$end)=split(/\t/);
-    print $_ . "\n";
+    my ($chr,$str,$end, $anno)=split(/\t/, $_, 4);
     for my $key(@{$regions{$chr}}){
         my @an=split(/\t/, $key);
         ##start gt anno start, start must also be lt anno end
         if($str>=$an[0] && $str<=$an[1]){
-            print OUT "$chr\t$str\t$end\t$an[2]\n";
+            print OUT "$chr\t$str\t$end\t$anno\t$an[2]\n";
             next;
         }
         ##start lt anno start, end must be gt anno start
         if($str<=$an[0] && $end>$an[0]){
-            print OUT "$chr\t$str\t$end\t$an[2]\n";
+            print OUT "$chr\t$str\t$end\t$anno\t$an[2]\n";
             next;
         }
     }
