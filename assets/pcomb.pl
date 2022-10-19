1d0e317803398886c1ca5db7420caca78b7255cb
brucemoran <bruce01campus@gmail.com>
2022-10-19 09:48:37 +0100

    pcomb.pl

--- assets/pcomb.pl
+++ assets/pcomb.pl
@@ -7,7 +7,7 @@ open(BED, $ARGV[0]);
 my %regions;
 while(<BED>){
     chomp;
-    my @sp=split(/\t/);
+    my @sp=split(/\t/, $_, 4);
     push(@{$regions{"$sp[0],$sp[1],$sp[2]"}},$sp[3]);
 }
 close BED;
@@ -16,9 +16,9 @@ open(BED, $ARGV[1]);
 open(OUT, ">$ARGV[2]");
 while(<BED>){
   chomp;
-  my ($chr,$str,$end,$anno)=split(/\t/);
+  my ($chr,$str,$end,$anno)=split(/\t/, $_, 4);
   for my $key(@{$regions{"$chr,$str,$end"}}){
-    print OUT "$chr\t$str\t$end\t$key;$anno\n";
+    print OUT "$chr\t$str\t$end\t$anno\t$key\n";
   }
 }
 
